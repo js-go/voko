2e97ff4ef4b2e25b6a274146820af9686da67c2f
eep <coolicer@foxmail.com>
2018-11-21 10:58:21 +0800

    fix: user controller test

--- test/app/controller/users.test.js
+++ test/app/controller/users.test.js
@@ -1,17 +1,24 @@
 'use strict'
 
 const { app, assert } = require('egg-mock/bootstrap')
+const { mysqlCleanUp } = require('../../dbSetup')
 
 describe('test/app/controller/users.test.js', () => {
-
+  let ctx
   const mock = {
     phone: '18899999999',
     password: 'test',
     username: 'test'
   }
+
+  before(async () => {
+    ctx = app.mockContext()
+  })
+
   after(async () => {
     await mysqlCleanUp(ctx)
   })
+
   it('should return 201 created.', async function () {
     await app.httpRequest()
       .post('/user/register')
@@ -20,7 +27,7 @@ describe('test/app/controller/users.test.js', () => {
       .then((response) => {
         assert(response.body.status == 201)
       })
-  });
+  })
 
   it('should return 500 error, phone has been used', async function () {
     await app.httpRequest()
@@ -31,7 +38,6 @@ describe('test/app/controller/users.test.js', () => {
         status: 500,
         message: 'phone has been used.'
       })
-  });
+  })
 
-
-});
+})
